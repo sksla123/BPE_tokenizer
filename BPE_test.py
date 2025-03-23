893f920ec0719fd43a88d1c23d477ae2ef494058
sksla123 <pjy010608@naver.com>
2025-03-23 12:02:10 +0900

    BPE 클래스 생성

--- BPE_test.py
+++ BPE_test.py
@@ -16,30 +16,55 @@ def get_kst_timestamp() -> str:
     kst_time = datetime.now(ZoneInfo("Asia/Seoul"))
     return kst_time.strftime("%Y%m%d%H%M%S")
 
-# pre-tokenize 함수 정의
 import re
+class BPE():
+    def __init__(self, config_data: dict):
+        self.config_data = config_data
+        self.train_corpus = config_data.get("train_corpus_path", None)
+        self.pre_tokenized_corpus = None
+        self.vocab_size = config_data.get("max_vocab", None)
+        self.vocab_save_path = config_data.get("vocab_output_path", None)
+        self.infer_vocab_path = config_data.get("infer_vocab_path", None)
+        self.input_data_path = config_data.get("input_data_path", None)
+        self.tokenized_result_path = config_data.get("tokenized_result_path", None)
+        self.vocab_dict = {}
 
-def pre_tokenize(corpus: str, method: str = "whitespace"):
-    '''
-    corpus (str): pre-tokenize 대상 코퍼스
-    method (str): pre-tokenize 방법 (whitespace, sentencepiece) # 현재는 whitespace만 지원(추후 개발)
-
-    return (list): pre-tokenize 결과
-    ''' 
-
-    if method == "whitespace":
-        return re.split(r'\s+', corpus) ## whitespace 기준으로 분리
-    else:
-        raise ValueError(f"지원하지 않는 pre-tokenize 방법입니다. {method}")
-
-# BPE 훈련 함수 정의
-def train_bpe(train_dataset: dict):
-    '''
-    train_dataset (dict): 훈련 데이터 설정
-
-    return (dict): 훈련 결과
-    '''
+    def _load_corpus(self, corpus_path: str):
+        with open(corpus_path, 'r') as f:
+            corpus = f.read()
+        return corpus
     
+    # pre-tokenize 함수 정의
+    def pre_tokenize(self, corpus: str, method: str = "whitespace"):
+        '''
+        corpus (str): pre-tokenize 대상 코퍼스
+        method (str): pre-tokenize 방법 (whitespace, sentencepiece) # 현재는 whitespace만 지원(추후 개발)
+
+        return (list): pre-tokenize 결과
+        ''' 
+
+        if method == "whitespace":
+            return re.split(r'\s+', corpus) ## whitespace 기준으로 분리
+        else:
+            raise ValueError(f"지원하지 않는 pre-tokenize 방법입니다. {method}")
+
+    # BPE 훈련 함수 정의
+    def train_bpe(self):
+        '''
+        train_dataset (dict): 훈련 설정 데이터
+
+        return (dict): 훈련 결과
+        '''
+        
+        logging.info("훈련에 사용할 말 뭉치를 로딩합니다.")
+        logging.debug(f"훈련 데이터 경로: {self.train_corpus}")
+        train_corpus = self._load_corpus(self.train_corpus)
+        
+        logging.info("Pre-Tokenization을 진행합니다.")
+        pre_tokenized_corpus = self.pre_tokenize(train_corpus)
+
+        logging.info("BPE 훈련을 진행합니다.")
+        
 
 ## 디버그용 로그 설정
 parser = argparse.ArgumentParser(prog="BPE Tokenizer")
@@ -77,10 +102,11 @@ logging.basicConfig(
 if not args.log:
     logging.disable(logging.CRITICAL)
 
+logging.debug(f"디버그 로깅 활성화")
 logging.info("로깅 활성화, BPE 토크나이저 프로그램 실행됨.")
 
 if args.train:
-    logging.info("모드 설정: train")
+    logging.debug("모드 설정: train")
     mode = "train"
 
     config_data = {
@@ -88,9 +114,9 @@ if args.train:
         "max_vocab": args.max_vocab,
         "vocab_output_path": args.vocab
     }
-    logging.info(f"훈련 데이터 설정: {config_data}")
+    logging.debug(f"훈련 데이터 설정: {config_data}")
 elif args.infer:
-    logging.info("모드 설정: infer")
+    logging.debug("모드 설정: infer")
     mode = "infer"
 
     config_data = {
@@ -98,14 +124,9 @@ elif args.infer:
         "input_data_path": args.input,
         "tokenized_result_path": args.output
     }
-    logging.info(f"추론 데이터 설정: {config_data}")
+    logging.debug(f"추론 데이터 설정: {config_data}")
 
 if mode == "train":
     logging.info("훈련 모드로 프로그램이 동작합니다.")
-    logging.info("훈련에 사용할 말 뭉치를 로딩합니다.")
-    with open(dataset["train_corpus_path"], 'r') as f:
-        train_corpus = f.read()
     
-    logging.info("Pre-Tokenization을 진행합니다.")
-    pre_tokenized_corpus = pre_tokenize(train_corpus)
 
